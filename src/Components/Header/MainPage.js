d56355a92a87141fe2d4387821cd1fd0e323af7d
muhammadmb <muhammadmagdyje@gmail.com>
2021-03-11 19:08:39 +0200

    design settings component

--- src/Components/Header/MainPage.js
+++ src/Components/Header/MainPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './MainPageStyle.css';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import bg1 from '../../Images/1.jpg';
@@ -7,49 +7,60 @@ import bg4 from '../../Images/4.jpg';
 import bg6 from '../../Images/6.jpg';
 import Settings from '../Settings/Settings';
 
-const MainPage = () =>{
+const MainPage = () => {
 
     const [value, setValue] = useState(0);
+    const [color, setColor] = useState(localStorage.getItem("color"));
+    const [randomBackground, setRandomBackground] = useState(true);
     const backgrounds = [bg1, bg3, bg4, bg6];
+    let background = backgrounds[value];
 
-    useEffect (() =>{
-        const interval = setInterval(() => {
-            setValue(Math.floor(Math.random() * backgrounds.length))
-        }, 10000);
-        return clearTimeout();
-    },[]);
+    useEffect(() => {
 
-    return(
-        <div className="main-page" style = {{backgroundImage:`url(${backgrounds[value]})`}}>
+        if (randomBackground) {
+            const interval = setInterval(() => {
+                setValue(Math.floor(Math.random() * backgrounds.length));
+            }, 5000);
+            return () => clearInterval(interval);
+        }
+    });
+
+    return (
+        <div className="main-page" style={{ backgroundImage: `url(${background})` }}>
 
             <div className="overlay">
                 <div className="header-area">
-                <Settings />
-                <nav className="navbar navbar-expand-lg">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <ReorderIcon fontSize="large"/>
-                    </button>
 
-                        <div className="collapse navbar-collapse" id="navbarContent">
+                    <Settings
+                        OnColorChange={(value) => setColor(value)}
+                        backgroundOptionsChange={(value) => setRandomBackground(!value)}
+                    />
+                    <div className="container">
+                        <nav className="navbar navbar-expand-lg">
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <ReorderIcon fontSize="large" />
+                            </button>
 
-                        <h1 className="brand">
-                            Equipments House
-                        </h1>
+                            <div className="collapse navbar-collapse" id="navbarContent">
 
-                        <ul className="links">
-                            <li className="nav-item"><a href="#1">About</a></li>
-                            <li className="nav-item"><a href="#1">Services</a></li>
-                            <li className="nav-item"><a href="#1">Products</a></li>
-                            <li className="nav-item"><a href="#1">Contact</a></li>
-                        </ul> 
+                                <h1 className="brand">
+                                    Equipments House
+                            </h1>
 
+                                <ul className="links">
+                                    <li className="nav-item"><a href="#1">About</a></li>
+                                    <li className="nav-item"><a href="#1">Services</a></li>
+                                    <li className="nav-item"><a href="#1">Products</a></li>
+                                    <li className="nav-item"><a href="#1">Contact</a></li>
+                                </ul>
+
+                            </div>
+                        </nav>
                     </div>
-                </nav>
-
                 </div>
 
                 <div className="introduction-text">
-                    <h1>Find your <span>Equipments</span></h1>
+                    <h1>Find your <span style={{ color }}>Equipments</span></h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum ipsum sit amet urna interdum pellentesque.</p>
                 </div>
 
--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,25 +1,66 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './SettingsStyle.css';
 import HouseIcon from '@material-ui/icons/House';
 import SettingsIcon from '@material-ui/icons/Settings';
+import Switch from '@material-ui/core/Switch';
 
-const Settings = () =>{
+const Settings = (props) => {
 
-    const[openSittngs, setOpenSettings] = useState(true);
+    const [openSittngs, setOpenSettings] = useState(true);
+    const [backgroundStatus, setBackgroundStatus] = useState(true);
 
-    return(
-        <div className= {openSittngs ? "settings-box" : "settings-box open"}>
-            <div 
-                className="toggle-settings" 
-                onClick = {() => setOpenSettings(!openSittngs)}
+    const color = localStorage.getItem("color");
+
+    const handleColorChange = (color) => {
+        localStorage.setItem("color", color)
+        props.OnColorChange(color);
+    }
+
+    const handleBackgroundController = () => {
+        setBackgroundStatus(!backgroundStatus);
+        props.backgroundOptionsChange(backgroundStatus);
+    }
+
+    return (
+        <div className={openSittngs ? "settings-box" : "settings-box open"}>
+            <div
+                className="toggle-settings"
+                onClick={() => setOpenSettings(!openSittngs)}
             >
-                <SettingsIcon 
-                    className={openSittngs ? "icon" : "settings-icon"} 
+                <SettingsIcon
+                    className={openSittngs ? "icon" : "settings-icon"}
                 />
             </div>
-            
-            <div className="settings-container">
-            <HouseIcon className="house-icon"/>
+
+            <div className="settings-content">
+
+                <HouseIcon className="house-icon" style={{ color }} />
+                <h4 className="settingsBrand" style={{ color }}>Equipments House</h4>
+
+                <div className="options-box">
+                    <h4>Colors</h4>
+                    <ul className="colors-list">
+                        <li className={color === "#FFD700" ? "active" : ""} onClick={() => handleColorChange("#FFD700")}></li>
+                        <li className={color === "#f06292" ? "active" : ""} onClick={() => handleColorChange("#f06292")}></li>
+                        <li className={color === "#004d40" ? "active" : ""} onClick={() => handleColorChange("#004d40")}></li>
+                        <li className={color === "#3e2723" ? "active" : ""} onClick={() => handleColorChange("#3e2723")}></li>
+                        <li className={color === "#263238" ? "active" : ""} onClick={() => handleColorChange("#263238")}></li>
+                    </ul>
+                </div>
+
+                <div className="options-box">
+                    <span className="span">Random background</span>
+                    <Switch
+                        style={{ color: localStorage.getItem("color") }}
+                        color="default"
+                        className="switch"
+                        checked={backgroundStatus}
+                        onChange={() => handleBackgroundController()}
+                        name="check randome background"
+                    />
+                    <div className="Clear"></div>
+                </div>
+
             </div>
         </div>
     );
